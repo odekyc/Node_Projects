d405643be2eef19ad24a0fee6c3864d5013c2093
odekyc <odekyc@gmail.com>
2016-09-19 02:20:12 +0000

    initial-commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express= require("express");
+
+var app=express();
+
+app.get("/", function(req, res){
+    
+    res.send("Hello World");
+});
+
+app.listen(8080, function(){
+    console.log("example uses port 8080");
+    
+});
